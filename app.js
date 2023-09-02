d58dbfa2cd2f8a7b6f419e6a7217aea6bdfd5583
Rishabhdhanwal10 <rdhanwal2000@gmail.com>
2023-09-03 03:41:13 +0530

    ecommerce api

---
+++ app.js
@@ -0,0 +1,22 @@
+// importing required packages
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./config/mongoose');
+
+require('dotenv').config({path: './config.env'});
+
+const PORT = 3000 || process.env.PORT;
+
+// initializing express
+const app = express();
+
+// using body parser to parse over the request body
+app.use(bodyParser.urlencoded({extended: true}));
+
+// using routes
+app.use('/products', require('./routes/products'));
+
+// starting the server
+app.listen(PORT, function(){
+    console.log('API is live on http://43.205.196.154:8000/products');
+});
